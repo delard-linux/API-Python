d0cd82db1591bfc754fce863e1f8c630594a2d42
EC2 Default User <ec2-user@ip-172-31-58-130.ec2.internal>
2021-02-07 12:09:27 +0000

    Se añade un fichero de test de los endpoints y ademas el serverless.yml modificado para uso en serverless.com

---
+++ test-endopoints.sh
@@ -0,0 +1,24 @@
+echo "INICIO TESTS de ENDPOINTS "
+
+echo " "
+echo "Test Create: "
+curl -X POST https://x0gyntrnw8.execute-api.us-east-1.amazonaws.com/dev/todos --data '{ "text": "Learn Serverless" }'
+
+echo " "
+echo "Test List: "
+curl https://x0gyntrnw8.execute-api.us-east-1.amazonaws.com/dev/todos
+
+echo " "
+echo "Test Get: "
+curl https://x0gyntrnw8.execute-api.us-east-1.amazonaws.com/dev/todos/5943a2f4-68b4-11eb-8691-8d3beed84eb6
+
+echo " "
+echo "Test Update: "
+curl -X PUT https://x0gyntrnw8.execute-api.us-east-1.amazonaws.com/dev/todos/5943a2f4-68b4-11eb-8691-8d3beed84eb6 --data '{ "text": "Learn python and more", "checked": true }'
+
+echo " "
+echo "Test Delete: "
+curl -X DELETE https://x0gyntrnw8.execute-api.us-east-1.amazonaws.com/dev/todos/6e9b9686-68b2-11eb-876d-ad870f88a010
+
+echo " "
+echo "FIN TESTS de ENDPOINTS "
